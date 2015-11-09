8bb89898ec5261ae87bcaee7a2465bf25eec2cfd
Megan Cunningham <megan.cunningham@smilecareclub.com>
2015-11-09 15:29:07 -0600

    celery tutorial all the things

--- picha/celery.py
+++ picha/celery.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picha.settings')
 app = Celery('picha')
@@ -16,3 +17,6 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+
